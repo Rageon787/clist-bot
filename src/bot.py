b216aabcc4708dcc524a8022b8c5c87ce9d176b6
Ragav Srinivasan <56784771+Rageon787@users.noreply.github.com>
2024-04-30 01:33:27 +1000

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -9,16 +9,28 @@ load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') 
 CLIST_TOKEN = os.getenv('CLIST_TOKEN') 
 CLIST_AUTHORIZATION = os.getenv('AUTHORIZATION') 
-URL = "https://clist.by:443/api/v4/contest/" + CLIST_AUTHORIZATION
+CONTEST_URL = "https://clist.by:443/api/v4/contest/" + CLIST_AUTHORIZATION
+RESOURCE_URL = "https://clist.by:443/api/v4/resource/" + CLIST_AUTHORIZATION
+
+
+platform_shorthand = {} 
 
 class MyClient(discord.Client): 
-    def input_cleaner(self, query_str) -> list[str]:
-        pass 
+    
+    def get_platforms(self): 
+        response = requests.get(RESOURCE_URL) 
+        data = response.json() 
+        for platform in data['objects']: 
+            platform_shorthand[platform['name']] = platform['short'] 
+    
     async def on_ready(self):
         print('bot is running') 
+        self.get_platforms() 
+    
     async def on_message(self, message):
         if message.author == self.user:
             return
+        user_query_author = message.author 
         user_query = message.content 
         user_query = user_query.split() 
         if user_query[0].startswith("$contests"):
@@ -26,16 +38,59 @@ class MyClient(discord.Client):
             max number contests shown -> 100 
             Note: only returns upcoming contests, for past contests use $past_contests 
             $contests (default) -> returns all upcoming contests 
-            $contests +timeframe -> returns all upcoming contests in the specified time frame 
             $contests -platform -> returns all upcoming contests in the specified platform 
             """
-            response = requests.get(URL + "&limit=5&upcoming=true") 
+            if user_query[0].startswith("$contests +cf") or user_query[0].startswith("$contests +codeforces"):
+                response = requests.get(CONTEST_URL + "&limit=100&upcoming=true&host=codeforces.com") 
+            elif user_query[0].startswith("$contests +cc") or user_query[0].startswith("$contests +codechef"):
+                response = requests.get(CONTEST_URL + "&limit=100&upcoming=true&host=codechef.com")
+            elif user_query[0].startswith("$contests +lc") or user_query[0].startswith("$contests + leetcode"):
+                response = requests.get(CONTEST_URL + "&limit=100&upcoming=true&host=leetcode.com") 
+            else: 
+                response = requests.get(CONTEST_URL + "&limit=100&upcoming=true") 
             if response.status_code == 200:
+                pages = [] 
                 data = response.json() 
-                result_message = "" 
-                for contest in data['objects']:
-                    result_message += f"{contest['event']} {contest['host']} \n" 
-                await message.channel.send(result_message)       
+                cnt = 0 
+                embedVar = discord.Embed(title="Future Contests | Page 1", color=0x00ff00) 
+                for contest in data['objects']: 
+                    field_name = f"{contest['event']}" 
+                    field_value = "[" + contest['host'] + "]" + "(" + contest['href'] + ")" 
+                    embedVar.add_field(name=field_name, value=field_value, inline=False) 
+                    cnt += 1 
+                    if cnt == 10:
+                        pages.append(embedVar) 
+                        cnt = 0 
+                        new_embedVar = discord.Embed(title=f"Future contests | Page {len(pages) + 1}", color=0x00ff00) 
+                        embedVar = new_embedVar 
+                if cnt > 0:
+                    pages.append(embedVar) 
+                current_page = 0 
+                message = await message.channel.send(embed=pages[current_page])  
+                
+                reactions = ['⬅️', '➡️']
+                for reaction in reactions:
+                    await message.add_reaction(reaction)
+                
+                def check(reaction, user):
+                    return user == user_query_author and str(reaction.emoji) in reactions 
+
+                while True:
+                    try:
+                        reaction, user = await self.wait_for('reaction_add', timeout=60.0, check=check)  
+                        if str(reaction.emoji) == '➡️':
+                            print("right") 
+                            current_page = (current_page + 1) % len(pages)
+                        elif str(reaction.emoji) == '⬅️':
+                            print("left") 
+                            current_page = (current_page - 1) % len(pages)
+
+                        await message.edit(embed=pages[current_page])
+                        await message.remove_reaction(reaction, user)
+
+                    except TimeoutError:
+                        break
+
             else:
                 print(f"failure: {response.status_code}")
             
@@ -47,7 +102,54 @@ class MyClient(discord.Client):
             $past_contests +timeframe -> returns all past contests in the specified time frame 
             $past_contests -platform -> returns all past contests in the specified platform  
             """
-        return 
+            response = requests.get(CONTEST_URL + "&limit=100&upcoming=false") 
+            if response.status_code == 200:
+                pages = [] 
+                data = response.json() 
+                cnt = 0 
+                embedVar = discord.Embed(title="Past Contests | Page 1", color=0x00ff00) 
+                for contest in data['objects']: 
+                    field_name = f"{contest['event']}" 
+                    field_value = "[" + contest['host'] + "]" + "(" + contest['href'] + ")" 
+                    embedVar.add_field(name=field_name, value=field_value, inline=False) 
+                    cnt += 1 
+                    if cnt == 10:
+                        pages.append(embedVar) 
+                        cnt = 0 
+                        new_embedVar = discord.Embed(title=f"Past contests | Page {len(pages) + 1}", color=0x00ff00) 
+                        embedVar = new_embedVar 
+                if cnt > 0:
+                    pages.append(embedVar) 
+                current_page = 0 
+                message = await message.channel.send(embed=pages[current_page])  
+                
+                reactions = ['⬅️', '➡️']
+                for reaction in reactions:
+                    await message.add_reaction(reaction)
+                
+                def check(reaction, user):
+                    return user == user_query_author and str(reaction.emoji) in reactions 
+
+                while True:
+                    try:
+                        reaction, user = await self.wait_for('reaction_add', timeout=60.0, check=check)  
+                        if str(reaction.emoji) == '➡️':
+                            print("right") 
+                            current_page = (current_page + 1) % len(pages)
+                        elif str(reaction.emoji) == '⬅️':
+                            print("left") 
+                            current_page = (current_page - 1) % len(pages)
+
+                        await message.edit(embed=pages[current_page])
+                        await message.remove_reaction(reaction, user)
+
+                    except TimeoutError:
+                        break
+
+            else:
+                print(f"failure: {response.status_code}")
+        
+          
     
 def main() -> None: 
     intents = discord.Intents.default() 
